9b08066bb3f815b9a973d54e201e12af21429cde
bibekkarki067 <bibekkarki067@gmail.com>
2020-12-01 12:43:29 -0600

    Compleated Autograd

---
+++ Assignments/Assignment_1/handout1/handout1/hw1/mnist.py
@@ -0,0 +1,49 @@
+"""Problem 3 - Training on MNIST"""
+import numpy as np
+
+# TODO: Import any mytorch packages you need (XELoss, SGD, etc)
+
+# NOTE: Batch size pre-set to 100. Shouldn't need to change.
+BATCH_SIZE = 100
+
+def mnist(train_x, train_y, val_x, val_y):
+    """Problem 3.1: Initialize objects and start training
+    You won't need to call this function yourself.
+    (Data is provided by autograder)
+    
+    Args:
+        train_x (np.array): training data (55000, 784) 
+        train_y (np.array): training labels (55000,) 
+        val_x (np.array): validation data (5000, 784)
+        val_y (np.array): validation labels (5000,)
+    Returns:
+        val_accuracies (list(float)): List of accuracies per validation round
+                                      (num_epochs,)
+    """
+    # TODO: Initialize an MLP, optimizer, and criterion
+
+    # TODO: Call training routine (make sure to write it below)
+    val_accuracies = None
+    return val_accuracies
+
+
+def train(model, optimizer, criterion, train_x, train_y, val_x, val_y, num_epochs=3):
+    """Problem 3.2: Training routine that runs for `num_epochs` epochs.
+    Returns:
+        val_accuracies (list): (num_epochs,)
+    """
+    val_accuracies = []
+    
+    # TODO: Implement me! (Pseudocode on writeup)
+    return val_accuracies
+
+def validate(model, val_x, val_y):
+    """Problem 3.3: Validation routine, tests on val data, scores accuracy
+    Relevant Args:
+        val_x (np.array): validation data (5000, 784)
+        val_y (np.array): validation labels (5000,)
+    Returns:
+        float: Accuracy = correct / total
+    """
+    #TODO: implement validation based on pseudocode
+    return 0
---
+++ Assignments/Assignment_1/handout1/handout1/mytorch/nn/functional.py
@@ -0,0 +1,283 @@
+import numpy as np
+
+import mytorch.tensor as tensor
+from mytorch.autograd_engine import Function
+
+
+def unbroadcast(grad, shape, to_keep=0):
+    while len(grad.shape) != len(shape):
+        grad = grad.sum(axis=0)
+    for i in range(len(shape) - to_keep):
+        if grad.shape[i] != shape[i]:
+            grad = grad.sum(axis=i, keepdims=True)
+    return grad
+
+class Transpose(Function):
+    @staticmethod
+    def forward(ctx, a):
+        if not len(a.shape) == 2:
+            raise Exception("Arg for Transpose must be 2D tensor: {}".format(a.shape))
+        requires_grad = a.requires_grad
+        b = tensor.Tensor(a.data.T, requires_grad=requires_grad,
+                                    is_leaf=not requires_grad)
+        return b
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return tensor.Tensor(grad_output.data.T)
+
+class Reshape(Function):
+    @staticmethod
+    def forward(ctx, a, shape):
+        if not type(a).__name__ == 'Tensor':
+            raise Exception("Arg for Reshape must be tensor: {}".format(type(a).__name__))
+        ctx.shape = a.shape
+        requires_grad = a.requires_grad
+        c = tensor.Tensor(a.data.reshape(shape), requires_grad=requires_grad,
+                                                 is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return tensor.Tensor(grad_output.data.reshape(ctx.shape)), None
+
+class Log(Function):
+    @staticmethod
+    def forward(ctx, a):
+        if not type(a).__name__ == 'Tensor':
+            raise Exception("Arg for Log must be tensor: {}".format(type(a).__name__))
+        ctx.save_for_backward(a)
+        requires_grad = a.requires_grad
+        c = tensor.Tensor(np.log(a.data), requires_grad=requires_grad,
+                                          is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        a = ctx.saved_tensors[0]
+        return tensor.Tensor(grad_output.data / a.data)
+
+"""EXAMPLE: This represents an Op:Add node to the comp graph.
+
+See `Tensor.__add__()` and `autograd_engine.Function.apply()`
+to understand how this class is used.
+
+Inherits from:
+    Function (autograd_engine.Function)
+"""
+class Add(Function):
+    @staticmethod
+    def forward(ctx, a, b):
+        # Check that both args are tensors
+        if not (type(a).__name__ == 'Tensor' and type(b).__name__ == 'Tensor'):
+            raise Exception("Both args must be Tensors: {}, {}".format(type(a).__name__, type(b).__name__))
+
+        # Check that args have same shape
+
+        # Save inputs to access later in backward pass.
+        ctx.save_for_backward(a, b)
+
+        # Create addition output and sets `requires_grad and `is_leaf`
+        # (see appendix A for info on those params)
+        requires_grad = a.requires_grad or b.requires_grad
+        c = tensor.Tensor(a.data + b.data, requires_grad=requires_grad,
+                                           is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # retrieve forward inputs that we stored
+        a, b = ctx.saved_tensors
+
+        # calculate gradient of output w.r.t. each input
+        # dL/da = dout/da * dL/dout
+        grad_a = np.ones(a.shape) * grad_output.data
+        # dL/db = dout/db * dL/dout
+        grad_b = np.ones(b.shape) * grad_output.data
+
+        # the order of gradients returned should match the order of the arguments
+        grad_a = tensor.Tensor(unbroadcast(grad_a, a.shape))
+        grad_b = tensor.Tensor(unbroadcast(grad_b, b.shape))
+        return grad_a, grad_b
+
+
+class Sub(Function):
+    @staticmethod
+    def forward(ctx, a, b):
+        # Check that inputs are tensors of same shape
+        if not (type(a).__name__ == 'Tensor' and type(b).__name__ == 'Tensor') or \
+            a.data.shape != b.data.shape:
+            pass
+        # Save inputs to access later in backward pass.
+        ctx.save_for_backward(a, b)
+
+        # Create addition output and sets `requires_grad and `is_leaf`
+        # (see appendix A for info on those params)
+        requires_grad = a.requires_grad or b.requires_grad
+        c = tensor.Tensor(a.data - b.data, requires_grad=requires_grad,
+                                           is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # retrieve forward inputs that we stored
+        a, b = ctx.saved_tensors
+
+        # calculate gradient of output w.r.t. each input
+        # dL/da = dout/da * dL/dout
+        grad_a = np.ones(a.shape) * grad_output.data
+        # dL/db = dout/db * dL/dout
+        grad_b = np.ones(b.shape) * -grad_output.data
+
+        # the order of gradients returned should match the order of the arguments
+        grad_a = tensor.Tensor(unbroadcast(grad_a, a.shape))
+        grad_b = tensor.Tensor(unbroadcast(grad_b, b.shape))
+        return grad_a, grad_b
+
+
+class Mul(Function):
+    @staticmethod
+    def forward(ctx, a, b):
+        # Check that inputs are tensors of same shape
+        if not (type(a).__name__ == 'Tensor' and type(b).__name__ == 'Tensor') or \
+            a.data.shape != b.data.shape:
+            pass
+        # Save inputs to access later in backward pass.
+        ctx.save_for_backward(a, b)
+
+        # Create addition output and sets `requires_grad and `is_leaf`
+        # (see appendix A for info on those params)
+        requires_grad = a.requires_grad or b.requires_grad
+        c = tensor.Tensor(a.data * b.data, requires_grad=requires_grad,
+                                           is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # retrieve forward inputs that we stored
+        a, b = ctx.saved_tensors
+
+        # calculate gradient of output w.r.t. each input
+        # dL/da = dout/da * dL/dout        
+        grad_a = np.array(b.data) * grad_output.data
+        # dL/db = dout/db * dL/dout
+        grad_b = np.array(a.data) * grad_output.data
+        # the order of gradients returned should match the order of the arguments
+        grad_a = tensor.Tensor(unbroadcast(grad_a, a.shape))
+        grad_b = tensor.Tensor(unbroadcast(grad_b, b.shape))
+        return grad_a, grad_b
+
+
+class Div(Function):
+    @staticmethod
+    def forward(ctx, a, b):
+        # Check that inputs are tensors of same shape
+        if not (type(a).__name__ == 'Tensor' and type(b).__name__ == 'Tensor') or \
+            a.data.shape != b.data.shape:
+            pass
+        # Save inputs to access later in backward pass.
+        ctx.save_for_backward(a, b)
+
+        # Create addition output and sets `requires_grad and `is_leaf`
+        # (see appendix A for info on those params)
+        requires_grad = a.requires_grad or b.requires_grad
+        c = tensor.Tensor(a.data / b.data, requires_grad=requires_grad,
+                                           is_leaf=not requires_grad)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # retrieve forward inputs that we stored
+        a, b = ctx.saved_tensors
+
+        # calculate gradient of output w.r.t. each input
+        # dL/da = dout/da * dL/dout        
+        grad_a = np.array(1/b.data) * grad_output.data
+        # dL/db = dout/db * dL/dout
+        grad_b = np.array(-a.data/b.data**2) * grad_output.data
+        # the order of gradients returned should match the order of the arguments
+        grad_a = tensor.Tensor(unbroadcast(grad_a, a.shape))
+        grad_b = tensor.Tensor(unbroadcast(grad_b, b.shape))
+        return grad_a, grad_b
+
+
+class Sum(Function):
+    @staticmethod
+    def forward(ctx, a, axis, keepdims):
+        if not type(a).__name__ == 'Tensor':
+            raise Exception("Only log of tensor is supported")
+        ctx.axis = axis
+        ctx.shape = a.shape
+        if axis is not None:
+            ctx.len = a.shape[axis]
+        ctx.keepdims = keepdims
+        requires_grad = a.requires_grad
+        c = tensor.Tensor(a.data.sum(axis = axis, keepdims = keepdims), \
+                          requires_grad=requires_grad, is_leaf=not requires_grad)
+        #print(a.shape, c.shape)
+        return c
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_out = grad_output.data
+
+        if (ctx.axis is not None) and (not ctx.keepdims):
+            grad_out = np.expand_dims(grad_output.data, axis=ctx.axis)
+        else:
+            grad_out = grad_output.data.copy()
+
+        grad = np.ones(ctx.shape) * grad_out
+
+        assert grad.shape == ctx.shape
+        # Take note that gradient tensors SHOULD NEVER have requires_grad = True.
+        return tensor.Tensor(grad), None, None
+
+
+
+# TODO: Implement more Functions below
+
+
+def cross_entropy(predicted, target):
+    """Calculates Cross Entropy Loss (XELoss) between logits and true labels.
+    For MNIST, don't call this function directly; use nn.loss.CrossEntropyLoss instead.
+
+    Args:
+        predicted (Tensor): (batch_size, num_classes) logits
+        target (Tensor): (batch_size,) true labels
+
+    Returns:
+        Tensor: the loss as a float, in a tensor of shape ()
+    """
+    batch_size, num_classes = predicted.shape
+
+    # Tip: You can implement XELoss all here, without creating a new subclass of Function.
+    #      However, if you'd prefer to implement a Function subclass you're free to.
+    #      Just be sure that nn.loss.CrossEntropyLoss calls it properly.
+
+    # Tip 2: Remember to divide the loss by batch_size; this is equivalent
+    #        to reduction='mean' in PyTorch's nn.CrossEntropyLoss
+
+    raise Exception("TODO: Implement XELoss for comp graph")
+
+def to_one_hot(arr, num_classes):
+    """(Freebie) Converts a tensor of classes to one-hot, useful in XELoss
+
+    Example:
+    >>> to_one_hot(Tensor(np.array([1, 2, 0, 0])), 3)
+    [[0, 1, 0],
+     [0, 0, 1],
+     [1, 0, 0],
+     [1, 0, 0]]
+
+    Args:
+        arr (Tensor): Condensed tensor of label indices
+        num_classes (int): Number of possible classes in dataset
+                           For instance, MNIST would have `num_classes==10`
+    Returns:
+        Tensor: one-hot tensor
+    """
+    arr = arr.data.astype(int)
+    a = np.zeros((arr.shape[0], num_classes))
+    a[np.arange(len(a)), arr] = 1
+    return tensor.Tensor(a, requires_grad = True)
+
